62b0dd3c5d79fdda8ab4c2b8ff1a4cb08e6b9df0
AnnoyingRains <annoyingrain5@gmail.com>
2024-03-06 12:29:30 +1100

    add unityhub

--- configuration.nix
+++ configuration.nix
@@ -183,6 +183,7 @@
     obs-studio
     cura
     nextcloud-client
+    unityhub # installed 2022.3.6f1 using the uri: unityhub://2022.3.6f1/b9e6e7e9fa2d
 
     # development (Crank It Up)
     dotnetCorePackages.sdk_6_0
