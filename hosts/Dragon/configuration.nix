9aa4d657788aab5f577c13fcf7c45c8846978790
AnnoyingRains <annoyingrain5@gmail.com>
2024-05-29 01:12:17 +1000

    Switch to beta drivers

--- hosts/Dragon/configuration.nix
+++ hosts/Dragon/configuration.nix
@@ -43,7 +43,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   fileSystems."/mnt/4tb_lmao" = {
