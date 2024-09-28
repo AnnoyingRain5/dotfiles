64657c1fe79fb8dd4a03987e3c0df0f8a87e92fb
AnnoyingRains <annoyingrain5@gmail.com>
2024-09-29 02:02:19 +1000

    Use CUDA for blender!

--- hosts/Dragon/configuration.nix
+++ hosts/Dragon/configuration.nix
@@ -7,6 +7,12 @@
   hardware.bluetooth.enable = true;
   hardware.bluetooth.settings.general.ControllerMode = "bredr";
 
+  nixpkgs.overlays = [
+    (self: super: {
+      blender = super.blender.override { cudaSupport = true; };
+    })
+  ];
+
   ### Nvidia ###
 
   # Enable OpenGL
@@ -48,7 +54,7 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = true;
+    open = false;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
